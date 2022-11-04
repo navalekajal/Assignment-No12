d8203436e8dfd7a5de54df8e6579cbee984636ce
navalekajal <113415224+navalekajal@users.noreply.github.com>
2022-11-04 19:25:41 +0530

    Add project files.

---
+++ Assignment No12/Bank.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_No12
+{
+    public delegate void MyDelegate();
+    public class Bank
+    {
+
+        public event MyDelegate insufficientbalance;
+        public event MyDelegate zerobalance;
+        public event MyDelegate lowbalance;
+        public event MyDelegate updatedbalance;
+        public int balance = 5000;
+        public int Credit(int amount)
+        {
+            balance = balance + amount;
+            //updatedbalance();
+            return balance;
+
+        }
+        public void Debit(int amount)
+        {
+            if (amount > balance)
+            {
+                insufficientbalance();
+            }
+            else
+            {
+                balance -= amount;
+
+
+                if (balance == 0)
+                {
+                    zerobalance();
+
+                }
+                else if (balance < 3000)
+                {
+                    lowbalance();
+
+                }
+                else
+                {
+                    Console.WriteLine(balance);
+                }
+            }
+        }
+        public class program
+        {
+            static void Imsg()
+            {
+                Console.WriteLine("you have insufficient balance");
+            }
+            static void Zmsg()
+            {
+                Console.WriteLine("you have zero balance");
+            }
+            static void Lmsg()
+            {
+                Console.WriteLine("you have low balance");
+            }
+            static void Main(string[] args)
+            {
+                Bank b = new Bank();
+                b.insufficientbalance += new MyDelegate(Imsg);
+                b.zerobalance += new MyDelegate(Zmsg);
+                b.lowbalance += new MyDelegate(Lmsg);
+                b.Credit(0);
+                b.Debit(3000);
+
+            }
+        }
+    }
+}
+
---
+++ Assignment No12/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_No12
+{
+    public delegate void MyDelegate();
+    public class student
+    {
+        public event MyDelegate Pass;
+        public event MyDelegate Fail;
+        public void AcceptMarks(int marks)
+        {
+            if (marks < 40)
+            {
+                Fail();
+            }
+            else
+            {
+                Pass();
+            }
+        }
+    }
+    public class Program
+    {
+        static void Passmsg()
+        {
+            Console.WriteLine("you are Pass");
+        }
+        static void Failmsg()
+        {
+            Console.WriteLine("you are Fail");
+        }
+        static void Main(string[] args)
+        {
+            student student = new student();
+            student.Pass += new MyDelegate(Passmsg);
+            student.Fail += new MyDelegate(Failmsg);
+            student.AcceptMarks(75);
+        }
+    }
+}
